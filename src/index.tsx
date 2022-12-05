0dbb9825e226bb563516854dd7f8bfb2fb076702
imzeze <jihyekim019@gmail.com>
2022-12-05 18:24:29 +0900

    feat: prettier eslint integration

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
     <>Hi</>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
