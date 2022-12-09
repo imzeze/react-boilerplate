76bf189beba79bbab16e93339c3e269da05b30ad
imzeze <jihyekim019@gmail.com>
2022-12-09 14:55:42 +0900

    feat: ts-loader 설정

--- webpack.config.ts
+++ webpack.config.ts
@@ -16,15 +16,12 @@ const config: Array<webpack.Configuration> = [
         {
           test: /\.(ts)x?$/,
           exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env',
-                ['@babel/preset-react', { runtime: 'automatic' }],
-              ],
-            },
-          },
+          use: 'babel-loader',
+        },
+        {
+          test: /\.(ts)x?$/,
+          exclude: /node_modules/,
+          use: 'ts-loader',
         },
       ],
     },
